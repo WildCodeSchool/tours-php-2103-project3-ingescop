3e51cf1c670576ba06a57afffe70e53386262e61
ThibaultBardinetLanglois <langlois.bardinet.thibault@gmail.com>
2021-06-04 10:03:27 +0200

    Creation of the website skeleton

---
+++ src/Controller/ProfessionnalController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+* @Route("/maître-d'oeuvre", name="professionnal_")
+*/
+
+class ProfessionnalController extends AbstractController
+{
+    /**
+     * @Route("/list", name="list")
+     */
+    public function list(): Response
+    {
+        return $this->render('professionnal/list.html.twig');
+    }
+}
