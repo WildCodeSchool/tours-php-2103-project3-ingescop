9d955170ec0814973acbc37ccc3cc53c92f9bfe4
ThibaultBardinetLanglois <langlois.bardinet.thibault@gmail.com>
2021-06-09 17:19:39 +0200

    Add a bubble effect for professionnal list and services list

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,26 @@ import './styles/app.css';
 
 // start the Stimulus application
 // import './bootstrap';
+
+// ====== CONST & GLOBAL VARIABLES ======
+const borderRadiusArray = [
+    '30% 60% 70% 40% / 50% 60% 30% 60%',
+    '60% 40% 30% 70% / 60% 30% 70% 40%',
+    '40% 60% 60% 40% / 60% 30% 70% 40%', '60% 40% 40% 60% / 30% 60% 40% 70%',
+    '60% 40%', '40% 60%',
+];
+
+function randomBorder() {
+    return borderRadiusArray[Math.floor(Math.random() * borderRadiusArray.length)];
+}
+
+const bubblesProfessionnals = document.querySelectorAll('.bubble-pro');
+const bubblesServices = document.querySelectorAll('.bubble-service');
+
+for (let i = 0; i < bubblesProfessionnals.length; i += 1) {
+    bubblesProfessionnals[i].style.borderRadius = randomBorder();
+}
+
+for (let i = 0; i < bubblesServices.length; i += 1) {
+    bubblesServices[i].style.borderRadius = randomBorder();
+}
