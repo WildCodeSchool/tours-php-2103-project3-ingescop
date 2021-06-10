8b396f7611e832db5c119b6a234d60df8afc24ac
Stéphane Sauvaget <s.sauvaget41000@gmail.com>
2021-06-10 23:14:15 +0200

    Modify contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,39 +2,42 @@
 
 namespace App\Controller;
 
-use App\Form\ContactType;
+
+use App\Form\ContactDataType;
+use App\FormData\ContactData;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/contactpro", name="contactpro")
      */
     public function index(Request $request, MailerInterface $mailer): Response
     {
-        $form = $this->createForm(ContactType::class);
+        $data = new ContactData();
+        $form = $this->createForm(ContactDataType::class, $data);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $contactFormData = $form->getData();
+            
 
             $message = (new Email())
-                ->from($contactFormData['email'])
-                ->to('contact@1gscop.fr')
+                ->from($data->getEmailAddress())
+                ->to('s.sauvaget41000@gmail.com')
                 ->subject('Vous avez reçu un nouveau mail')
-                ->text('Send by : ' . $contactFormData['email'] . \PHP_EOL .
-                 $contactFormData['message'], 'text/plain');
+                ->text('Send by : ' . $data->getEmailAddress() . \PHP_EOL .
+                 $data->getMessage(), 'text/plain');
                  $mailer->send($message);
 
-                 $this->addFlash("Parfait", 'Votre message à été envoyé avec succès !');
-                 return $this->redirectToRoute('contact');
+                 $this->addFlash("Success", 'Votre message à été envoyé avec succès !');
+                 return $this->redirectToRoute('contactpro');
         }
-        return $this->render('contact/index.html.twig', [
+        return $this->render('contact/contactpro.html.twig', [
             'form' => $form->createView()
         ]);
     }
---
+++ src/FormData/ContactData.php
@@ -0,0 +1,92 @@
+<?php
+
+declare (strict_types=1);
+
+namespace App\FormData;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ContactData
+{   
+    /**
+     * @Assert\NotBlank
+     */
+    private string $firstName;
+
+    /**
+     * @Assert\NotBlank
+     */
+    private string $lastName;
+
+    /**
+     * @Assert\NotBlank
+     */
+    private string $phoneNumber;
+
+    /**
+     * @Assert\Email
+     * @Assert\NotBlank
+     */
+    private string $emailAddress;
+
+    /**
+     * @Assert\NotBlank
+     */
+    private string $message;
+
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    public function getPhoneNumber(): string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(string $phoneNumber): self
+    {
+        $this->phoneNumber = $phoneNumber;
+        return $this;
+    }
+
+    public function getEmailAddress(): string
+    {
+        return $this->emailAddress;
+    }
+
+    public function setEmailAddress(string $emailAddress): self
+    {
+        $this->emailAddress = $emailAddress;
+        return $this;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+        return $this;
+    }
+}
