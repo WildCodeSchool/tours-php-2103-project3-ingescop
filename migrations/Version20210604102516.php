84473dd889c83b5f42350356c682980736f884b0
Romain Clair <romain.clair@wildcodeschool.fr>
2021-06-14 11:01:06 +0200

    Clean migration files

--- migrations/Version20210604102516.php
+++ migrations/Version20210604102516.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210614073259 extends AbstractMigration
+final class Version20210604102516 extends AbstractMigration
 {
     public function getDescription(): string
     {
