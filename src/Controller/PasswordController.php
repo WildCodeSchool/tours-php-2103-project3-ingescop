29eb99049ec9ebf31155e4f85341e54568572e2d
cedric <cedric-perrotin_student2021@wilder.school>
2021-07-06 16:21:22 +0200

    add change password for admin

---
+++ src/Controller/PasswordController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Admin;
+use App\FormData\PasswordData;
+use App\Form\ResetPasswordType;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+
+class PasswordController extends AbstractController
+{
+    /**
+     * @Route("/resetPassword", name="reset-password")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function edit(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $result = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+        if (!$user instanceof Admin) {
+            throw $this->createAccessDeniedException('vous devez etre connecté');
+        }
+        $changePassword = new PasswordData();
+        // rattachement du formulaire avec la class PasswordData
+        $form = $this->createForm(ResetPasswordType::class, $changePassword);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newpwd = $form->get('Password')['first']->getData();
+            $newEncodedPassword = $passwordEncoder->encodePassword($user, $newpwd);
+            $user->setPassword($newEncodedPassword);
+            $result->flush();
+            $this->addFlash('notice', 'Votre mot de passe à bien été changé !');
+            return $this->redirectToRoute('admin_panelconfig');
+        } else {
+            $form->addError(new FormError('Ancien mot de passe incorrect'));
+        }
+
+        return $this->render('password/index.html.twig', array(
+                    'form' => $form->createView(),
+                    'user' => $user
+        ));
+    }
+}
