e4dad75186a0bab1eae83bada1abfc0f6fc446a3
cedric <cedric-perrotin_student2021@wilder.school>
2021-07-08 12:10:05 +0200

    modif fixtures

--- src/DataFixtures/ProfessionnalFixtures.php
+++ src/DataFixtures/ProfessionnalFixtures.php
@@ -14,42 +14,50 @@ class ProfessionnalFixtures extends Fixture
         // $manager->persist($product);
         $professionnal = new Professionnal();
         $professionnal->setName('Alice PERROTE')
-            ->setJob('');
+            ->setJob('')
+            ->setResume('');
         $manager->persist($professionnal);
 
         $professionnal1 = new Professionnal();
         $professionnal1->setName('Robert BORIA')
-            ->setJob('');
+            ->setJob('')
+            ->setResume('');
         $manager->persist($professionnal1);
 
         $professionnal2 = new Professionnal();
         $professionnal2->setName('Patrick GARREAU')
-            ->setJob('');
+            ->setJob('')
+            ->setResume('');
         $manager->persist($professionnal2);
 
         $professionnal3 = new Professionnal();
         $professionnal3->setName('Sandra GERART')
-            ->setJob('');
+            ->setJob('')
+            ->setResume('');
         $manager->persist($professionnal3);
 
         $professionnal4 = new Professionnal();
         $professionnal4->setName('Etienne')
-            ->setJob('');
+            ->setJob('')
+            ->setResume('');
         $manager->persist($professionnal4);
 
         $professionnal5 = new Professionnal();
         $professionnal5->setName('Louis')
-            ->setJob('');
+            ->setJob('')
+            ->setResume('');
         $manager->persist($professionnal5);
 
         $professionnal6 = new Professionnal();
         $professionnal6->setName('Jean-Claude LEMMEL')
-            ->setJob('');
+            ->setJob('')
+            ->setResume('');
         $manager->persist($professionnal6);
 
         $professionnal7 = new Professionnal();
         $professionnal7->setName('Stéphane GRIFFE')
-            ->setJob('');
+            ->setJob('')
+            ->setResume('');
         $manager->persist($professionnal7);
 
         $manager->flush();
