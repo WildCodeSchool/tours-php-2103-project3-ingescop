0624e1d0fe31329e9acd92b8c0cb3e4c6f0353f0
ThibaultBardinetLanglois <langlois.bardinet.thibault@gmail.com>
2021-07-09 11:47:42 +0200

    the responsive is always a chore

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,6 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addStyleEntry('css/front', ['./assets/styles/header_footer.css'])
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
