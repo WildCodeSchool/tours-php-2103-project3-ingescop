88359b055d90570a1a94cbd0d45a526f73fd894c
cedric <cedric-perrotin_student2021@wilder.school>
2021-07-11 17:09:24 +0200

    change password css

--- src/Form/ResetPasswordType.php
+++ src/Form/ResetPasswordType.php
@@ -15,17 +15,21 @@ class ResetPasswordType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-                ->add('oldPassword', PasswordType::class)
+                ->add('oldPassword', PasswordType::class, [
+                    'label' => 'Ancien mot de passe',
+                ])
                 ->add('Password', RepeatedType::class, array(
                     'type' => PasswordType::class,
-                    'first_options' => ['label' => 'Password'],
-                    'second_options' => ['label' => 'RepeatPassword'],
+                    'first_options' => ['label' => 'Nouveau mot de passe',
+                                        'label_attr' => ['class' => 'text-5xl text-gray-600']],
+                    'second_options' => ['label' => 'Retapez le nouveau mot de passe',
+                                         'label_attr' => ['class' => 'text-5xl text-gray-600']],
                     'invalid_message' => 'Les deux mots de passe doivent être identiques',
-                    'options' => array(
-                        'attr' => array(
-                            'class' => 'password-field'
-                        )
-                    ),
+                    'options' => [
+                        'attr' => [
+                            'class' => 'shadow appearance-none border-yellow-200 h-15 w-32 p-4 border-4 w-full'
+                        ]
+                    ],
                     'required' => true,
                 ));
     }
