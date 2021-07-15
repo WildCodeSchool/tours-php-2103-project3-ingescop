66dd11c3a3b15ff87ca2392e39ca594a97458963
Stéphane Sauvaget <s.sauvaget41000@gmail.com>
2021-07-15 11:46:05 +0200

    Add a first admin and add professionnal in service vue

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Admin;
 use App\Entity\Partner;
+use App\Repository\AdminRepository;
 use App\Repository\PartnerRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(PartnerRepository $partnerRepository): Response
-    {
+    public function index(
+        PartnerRepository $partnerRepository,
+        UserPasswordEncoderInterface $passwordEncoder,
+        EntityManagerInterface $manager,
+        AdminRepository $adminRepository
+    ): Response {
+        $admin = $adminRepository->findAll();
+        if (empty($admin)) {
+            $firstAdmin = new Admin();
+            $firstAdmin->setUsername('admin');
+            $firstAdmin->setRoles(['ROLE_ADMIN']);
+            $firstAdmin->setPassword($passwordEncoder->encodePassword(
+                $firstAdmin,
+                'patrickgarreau123@'
+            ));
+
+            $manager->persist($firstAdmin);
+            $manager->flush();
+        }
+
         return $this->render('home/index.html.twig', [
             'partners' => $partnerRepository->findAll()
         ]);
